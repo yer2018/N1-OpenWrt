90a21188e932537d7c1ad2e931cf73f786aa9c32
yer2018 <42699536+yer2018@users.noreply.github.com>
2025-07-21 19:10:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,10 +2,11 @@
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 git clone https://github.com/vernesong/OpenClash.git --depth=1 clone/openclash
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-openclash
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/openclash/luci-app-openclash feeds/luci/applications/
 
 # Clean packages
