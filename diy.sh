e7111cc50243d08aee6d1328a7f6b2ee7f183a0d
yer2018 <42699536+yer2018@users.noreply.github.com>
2025-07-23 05:40:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 10.10.10.1）
+sed -i 's/192.168.1.1/10.10.10.100/g' package/base-files/files/bin/config_generate
+
+# Modify system hostname（FROM OpenWrt CHANGE TO OpenWrt-N1）
+sed -i 's/immortalwrt/Phicomm-N1/g' package/base-files/files/bin/config_generate
+
+# Replace the default software source
+# sed -i 's#mirrors.vsean.net#mirrors.bfsu.edu.cn#g' package/lean/default-settings/files/zzz-default-settings
+
+sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/\.apdisk\//\.apdisk\/\._*/g' feeds/packages/net/samba4/files/smb.conf.template
+
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 #git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
