62cfb4db112de52acfc263b6c23837386ebfd58b
yer2018 <42699536+yer2018@users.noreply.github.com>
2025-07-23 03:50:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -4,8 +4,8 @@
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 #git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
 git clone https://github.com/vernesong/OpenClash.git --depth=1 clone/openclash
-git clone https://github.com/jerrykuku/luci-app-argon-config.git clone/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-theme-argon.git clone/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git --depth=1 clone/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git --depth=1 clone/luci-theme-argon
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
